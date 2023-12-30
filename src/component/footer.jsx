ebe63ea066e68dcf2be14f5467a4dc2678fa2fb3
fauzanikhsani <fauzanikhsani@student.telkomuniversity.ac.id>
2023-12-30 12:56:01 +0700

    add backend

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,5 +1,48 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Footer() {
+    const [data, setData] = useState({
+        nama: '',
+        email: '',
+        isi: '',
+    });
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const datas = {
+            nama : data.nama,
+            email : data.email,
+            isi : data.isi,
+        }
+        try {
+            const response = await axios.post('http://localhost:3000/kritik', datas, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (response.status == 200) {
+                setData({
+                    nama: '',
+                    email: '',
+                    isi: '',
+                });
+            }else{
+                console.error("teteyy");
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
     return (
         <footer>
             <div id="footer1">
@@ -21,29 +64,49 @@ function Footer() {
                         </div>
                         <div className="col-12 col-md-6 mt-5">
                             <h2>KRITIK DAN SARAN</h2>
-                            <form>
+                            <form onSubmit={handleSubmit}>
                                 <div className="row">
                                     <div className="col-10">
                                         <div className="form-group">
                                             <label htmlFor="nama">Nama:</label>
-                                            <input type="text" name="nama" className="form-control" required />
-                                            <label htmlFor="nama">email:</label>
-                                            <input type="text" name="email" className="form-control" required />
-                                            <label htmlFor="nama">kritik & Saran:</label>
-                                            <input type="text" name="saran" className="form-control" required />
+                                            <input
+                                                type="text"
+                                                name="nama"
+                                                className="form-control"
+                                                value={data.nama}
+                                                onChange={handleChange}
+                                                required
+                                            />
+                                            <label htmlFor="email">Email:</label>
+                                            <input
+                                                type="text"
+                                                name="email"
+                                                className="form-control"
+                                                value={data.email}
+                                                onChange={handleChange}
+                                                required
+                                            />
+                                            <label htmlFor="isi">Kritik & Saran:</label>
+                                            <input
+                                                type="text"
+                                                name="isi"
+                                                className="form-control"
+                                                value={data.saran}
+                                                onChange={handleChange}
+                                                required
+                                            />
                                         </div>
                                     </div>
                                     <div className="col-2">
-                                        <button type="submit" className="btn btn-success my-3" style={{ height: '130px' }}>Kirim</button>
+                                        <button type="submit" className="btn btn-success my-3" style={{ height: '130px' }}>
+                                            Kirim
+                                        </button>
                                     </div>
-
                                 </div>
-
                             </form>
                         </div>
                     </div>
                 </div>
-
             </div>
         </footer>
     );
@@ -51,10 +114,7 @@ function Footer() {
 
 const linkStyle = {
     color: 'green',
-    textDecoration: 'none',  // Menghapus garis bawah
+    textDecoration: 'none',
 };
 
-
 export default Footer;
-
-
